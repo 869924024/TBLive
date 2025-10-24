e0dba9da452a51e9fdadfc3c3e6eadd9851d3091
huangjiajia <869924024@qq.com>
2025-10-25 01:02:31 +0800

    feat ： 直播项目完成

---
+++ model/device.py
@@ -0,0 +1,9 @@
+
+class Device:
+    def __init__(self, devid, miniwua, sgext, umt, utdid):
+        self.devid = devid
+        self.miniwua = miniwua
+        self.sgext = sgext
+        self.umt = umt
+        self.utdid = utdid
+        self.ttid = "1568860058617@taobao_android_10.51.0"
